e5c938244c3d1a87b25273c4ba5de18c5156305b
vadim11041996 <vadim11041996@hotmail.com>
2018-04-15 23:36:41 +0300

    download file by id

---
+++ api/src/models/file.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+
+class File {
+  constructor(app){
+    this.app = app;
+    this.model = {
+      name:null,
+      originalName:null,
+      mimeType:null,
+      size:null,
+      created:Date.now()
+    }
+  }
+
+  initWithObject(object){
+    this.model.name= _.get(object, 'filename');
+    this.model.originalName= _.get(object, 'originalname');
+    this.model.mimeType = _.get(object, 'mimetype');
+    this.model.size = _.get(object,'size');
+    this.model.created = Date.now();
+
+    return this;
+  }
+
+  toJson(){
+    return this.model;
+  }
+
+  // save(callback){
+  //   const db = this.app.get('db');
+  //   db.collection('files').insertOne(this.model,(err, result) =>{
+  //     return callback(err,result);
+  //   });
+  // }
+}
+
+export default File;
--- api/src/router.js
+++ api/src/router.js
@@ -1,5 +1,8 @@
 import path from 'path';
 import {author} from '../package.json';
+import _ from 'lodash';
+import File from './models/file';
+import {ObjectID} from 'mongodb';
 
 class AppRouter {
     constructor(app){
@@ -9,6 +12,7 @@ class AppRouter {
 
     setupRouters(){
       const app = this.app;
+      const db = app.get('db');
       const uploadDir = app.get('storageDir');
       const upload = app.get('upload');
 
@@ -22,27 +26,67 @@ class AppRouter {
       //Upload routind
       app.post('/api/upload', upload.array('files'), (req, res, next) =>{
         //console.log("file received ", req.files);
-        const files = req.files;
-        return res.json({
-          files:files,
-        })
+        const files = _.get(req, 'files',[]);
+
+        let fileModels =[];
+
+        _.each(files, (fileObject) =>{
+          const newFile = new File(app).initWithObject(fileObject).toJson();
+          fileModels.push(newFile);
+        });
+
+        if(fileModels.length){
+          db.collection('files').insertMany(fileModels, (err, result) =>{
+            if(err){
+              return res.status(503).json({
+                error:{
+                  message:'Unable saved your files',
+                }
+              });
+            }
+            console.log("save files with result", err, result);
+            return res.json({
+              files:fileModels
+            });
+          });
+        }else {
+          return res.status(503).json({
+            error:{message:'File upload is required'}
+          });
+        }
+
       });
 
       //Download routing
-      app.get('/api/download/:name', (req, res, next) =>{
-        const fileName = req.params.name;
-        const filePath = path.join(uploadDir, fileName);
-        return res.download(filePath, fileName, (err) =>{
-          if(err){
-            return res.status(404).json({
+      app.get('/api/download/:id', (req, res, next) =>{
+        const fileId = req.params.id;
+
+        db.collection('files').find({_id:ObjectID(fileId)}).toArray((err,result) => {
+          const fileName = _.get(result,'[0].name');
+
+          if(err || !fileName){
+            return res.status(400).json({
               error:{
                 message:"File not found"
               }
-            });
-          }else {
-            console.log("File is downloaded");
+            })
           }
+
+          const filePath = path.join(uploadDir,fileName);
+          return res.download(filePath, fileName, (err)=>{
+            if(err){
+              return res.status(404).json({
+                error:{
+                  message:'File not found'
+                }
+              });
+            }else{
+              console.log('File is downloaded.')
+            }
+          });
         });
+
+
       });
 
     }
