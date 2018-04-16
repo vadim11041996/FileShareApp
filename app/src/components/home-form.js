1d8e8b560b7d1c1d02026ff60309f70d9c9d6760
vadim11041996 <vadim11041996@hotmail.com>
2018-04-16 18:52:02 +0300

    can upload images

--- app/src/components/home-form.js
+++ app/src/components/home-form.js
@@ -1,19 +1,147 @@
 import React,{Component} from 'react';
+import _ from 'lodash';
 
 export default class HomeForm extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+      form:{
+        files: [],
+        to:'',
+        from:'',
+        message:''
+      },
+
+      errors:{
+        to: null,
+        from:null,
+        message:null,
+
+      }
+    };
+    this._onTextChange = this._onTextChange.bind(this);
+    this._onSubmit = this._onSubmit.bind(this);
+    this._formValidation = this._formValidation.bind(this);
+    this._onFileAdded = this._onFileAdded.bind(this);
+  }
+
+  _onFileAdded(event){
+    let files = [];
+    _.each(_.get(event, 'target.files', []), (file) =>{
+      files.push(file);
+    });
+
+    this.setState({
+      form:{
+          ...this.state.form,
+          files: files,
+      }
+    });
+
+  // console.log('files', files);
+  }
+
+  _isEmail(emailAddress){
+      const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      return emailRegex.test(emailAddress);
+  }
+
+  _formValidation(fields = [], callback){
+    let {form, errors} = this.state;
+    const validations = {
+
+      from:[
+        {
+          errorMessage:'From is required.',
+          isValid:() =>{
+            return form.from.length;
+          }
+        },
+        {
+          errorMessage:'Email is not valid',
+          isValid:() =>{
+            return this._isEmail(form.from);
+          }
+        }
+      ],
+
+      to:[
+        {
+          errorMessage:'To is required.',
+          isValid:() =>{
+            return form.to.length;
+          }
+        },
+        {
+          errorMessage:'Email is not valid',
+          isValid:() =>{
+            return this._isEmail(form.to);
+          }
+        }
+      ]
+
+    }
+
+    _.each(fields, (field) =>{
+      let fieldValidations = _.get(validations, field, []);
+      errors[field] = null;
+
+      _.each(fieldValidations, (fieldValidation) =>{
+        const isValid = fieldValidation.isValid();
+        if(!isValid){
+          errors[field] = fieldValidation.errorMessage;
+        }
+      });
+    });
+
+    this.setState({
+      errors: errors
+    }, () => {
+      let isValid = true;
+      _.each(errors, (err) =>{
+        if(err != null){
+          isValid = false;
+        }
+      });
+      return callback(isValid);
+    })
+
+  }
+
+  _onSubmit(event){
+    event.preventDefault();
+    this._formValidation(['from','to'], (isValid) =>{
+      console.log('The form is valid?', isValid);
+    });
+  }
+
+  _onTextChange(event){
+
+    let {form} = this.state;
+    const fieldName = event.target.name;
+    const fieldValue = event.target.value;
+
+    form[fieldName] = fieldValue;
+    this.setState({form: form});
+  }
+
   render(){
+    const {form} = this.state;
+
     return(
       <div className={'app-card'}>
-        <form >
+        <form onSubmit={this._onSubmit}>
           <div className={'app-card-header'}>
             <div className={'app-card-header-inner'}>
               <div className={'app-file-select-zone'}>
-              <label className={"label1"} htmlFor={'inputfile'}>
-                <input className={"input1"} id={'input-file'} type='file' multiple={true} />
-                <span className={'app-upload-icon'} />
-                <span className={'app-upload-description'}>Drag and drop your files here.</span>
-              </label>
-            </div>
+                <label className={"label1"} htmlFor={'input-file'}>
+                  <input onChange={this._onFileAdded} className={"input1"} id={'input-file'} type='file' multiple={true} />
+                  <span className={'app-upload-icon'} />
+                  <span className={'app-upload-description'}>Drag and drop your files here.</span>
+                </label>
+              </div>
 
             </div>
           </div>
@@ -21,18 +149,23 @@ export default class HomeForm extends Component{
             <div className={'app-card-content-inner'}>
               <div className={'app-form-item'}>
                 <label className={"label2"} htmlFor={'to'}>Send to</label>
-                <input className={"input2"} name={'to'} placeholder={'Email adress'} type={'text'} id={'to'} />
+                <input onChange={this._onTextChange} value={form.to} className={"input2"} name={'to'} placeholder={'Email adress'} type={'text'} id={'to'} />
               </div>
 
               <div className={'app-form-item'}>
                 <label className={"label3"} htmlFor={'from'}>From</label>
-                <input className={"input3"} name={'from'} placeholder={'Your email adress'} type={'text'} id={'from'} />
+                <input onChange={this._onTextChange} className={"input3"} name={'from'} placeholder={'Your email adress'} type={'text'} id={'from'} />
               </div>
 
               <div className={'app-form-item'}>
                 <label className={"label4"} htmlFor={'message'}>Message</label>
-                <textarea className={"textarea1"} id={'message'} placeholder={'Add a note (optional)'} name={'message'}/>
+                <textarea onChange={this._onTextChange} className={"textarea1"} id={'message'} placeholder={'Add a note (optional)'} name={'message'}/>
               </div>
+
+              <div className={'app-form-actions'}>
+                <button type={'submit'} className={'app-button primary'}>Send</button>
+              </div>
+
             </div>
           </div>
         </form>
